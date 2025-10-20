76b762efb597f2b21f8e3ed9e353423b99fe6c41
Essence <alanajohnson10012@gmail.com>
2025-10-20 14:33:20 -0400

    Add files via upload

---
+++ RcodeMilestone5.R
@@ -0,0 +1,19 @@
+View(twitchdata_update)
+#Watchtime data
+mean(twitchdata_update$`Watch time(Minutes)`)
+#Mean= 418427930
+median(twitchdata_update$`Watch time(Minutes)`)
+#Median= 234990788
+var(twitchdata_update$`Watch time(Minutes)`)
+#Var= 3.020992e+17
+sd(twitchdata_update$`Watch time(Minutes)`)
+#Sd= 549635514
+#Average Viewers data
+mean(twitchdata_update$`Average viewers`)
+#Mean= 4781.04
+median(twitchdata_update$`Average viewers`)
+#Median= 2425
+var(twitchdata_update$`Average viewers`)
+#Var= 71464789
+sd(twitchdata_update$`Average viewers`)
+#Sd= 8453.685
